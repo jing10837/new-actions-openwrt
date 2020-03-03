4c690e8fc2264bebf60552cfefe8d83f8bc4bd63
jing10837 <510422336@qq.com>
2020-03-03 14:54:23 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,7 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://e.coding.net/Small_5/luci-app-adbyby-plus-ram.git Package/luci-app-adbyby-plus-ram
-git clone https://github.com/jefferymvp/luci-app-koolproxyR.git Package/luci-app-koolproxyR
+git clone https://e.coding.net/Small_5/luci-app-adbyby-plus-ram.git package/luci-app-adbyby-plus-ram
+git clone https://github.com/jefferymvp/luci-app-koolproxyR.git package/luci-app-koolproxyR
 ./scripts/feeds update -a
 ./scripts/feeds install -a
